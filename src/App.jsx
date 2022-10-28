9657da44285cc25b33dbb16d489f65f3df0626a6
samwpr <swpr10x@gmail.com>
2022-10-28 16:44:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+//https://www.youtube.com/watch?v=YnnQymvBrY8&ab_channel=TylerPotts
+//13:40
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col text-white">
+      <main className="container mx-auto px-6 pt-6 flex-1 text-center">
+
+        <h2 className="text-2xl md:text-4xl lg:text-4xl uppercase">
+          Welcome to
+        </h2>
+
+        <h1 className="text-3xl md:text-5xl lg:text-5xl font-black uppercase mb-8">
+          Finance Co-pilot
+        </h1>
+
+
+
+
+
+
+      
+
+
+
+
+      </main>
+
+      <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">  
+        <p>Built with 💙 by Samuel</p>
+        <div className="flex -mx-6">
+          <a href="#" className="mx-3 hover:opacity-80 duration-150">About</a>
+
+          <a href="#" className="mx-3 hover:opacity-80 duration-150">Privacy</a>
+
+          <a href="#" className="mx-3 hover:opacity-80 duration-150">Contact</a>
+          
+
+
+        </div>
+        
+      </footer>
+      
+    </div>
+  )
+
+}
+
+export default App
